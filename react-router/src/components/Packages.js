6d5a3db0e14f42fb9f922dfdb917649026884008
Wendy Wang <wendywangg928@gmail.com>
2023-08-22 18:28:32 -0400

    added bootstrap to all components

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,3 +1,7 @@
+import ListGroup from "react-bootstrap/ListGroup";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+
 export default function Packages(props) {
   const displayPackages = props.packages.map((eachPackage) => (
     <li>{eachPackage}</li>
@@ -6,10 +10,18 @@ export default function Packages(props) {
   return (
     <div>
       <div className="packages">
-        <div className="packagesHeader">
-          <h1>Our Packages</h1>
-        </div>
-        <ul>{displayPackages}</ul>
+        <Container>
+          <Card>
+            <Card.Body>
+              <Card.Title>Our Packages</Card.Title>
+              <Card.Text>
+                Check out some of our packages! Every package is ethically
+                sourced and organic!
+              </Card.Text>
+            </Card.Body>
+            <ListGroup>{displayPackages}</ListGroup>
+          </Card>
+        </Container>
       </div>
     </div>
   );
